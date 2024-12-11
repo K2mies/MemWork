4f56b485b84d0528d5a666b21a03c14ad9fdd523
Ross Hvidsten <rhvidste@student.hive.fi>
2024-12-11 16:24:42 +0200

    added print_bits

---
+++ print_bits/rs_print_bits00.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rs_print_bits00.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/11 16:16:17 by rhvidste          #+#    #+#             */
+/*   Updated: 2024/12/11 16:23:59 by rhvidste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+void	print_bits(unsigned char octet)
+{
+	int	i = 8;
+	unsigned char	bit;
+
+	while (i--)
+	{
+		bit = (octet >> i & 1) + '0';
+		write(1, &bit, 1);
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	int	src;
+
+	src = atoi(argv[1]);
+	print_bits(src);
+}
