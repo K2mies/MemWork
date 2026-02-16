76d3a29be2a49fe6245fb177b04b338319d91f76
K2Mies <rhvidste@student.hive.fi>
2026-02-16 21:39:57 +0200

    more polyset practive before bed

---
+++ rank_5_exam/polyset/v04/searchable_array_bag.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   searchable_array_bag.cpp                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhvidste <rvidste@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 21:09:29 by rhvidste          #+#    #+#             */
+/*   Updated: 2026/02/16 21:16:50 by rhvidste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"searchable_array_bag.hpp"
+
+searchable_array_bag::searchable_array_bag(){}
+searchable_array_bag::searchable_array_bag( const searchable_array_bag& source ) : array_bag( source ) {}
+
+searchable_array_bag&	searchable_array_bag::operator	=	( const searchable_array_bag& source )
+{
+	if ( this != &source )
+		array_bag::operator=( source );
+	return ( *this );
+}
+
+bool	searchable_array_bag::has	( int value )	const
+{
+	for ( int i = 0; i < this->size; i++)
+	       if ( this->data[i] == value )
+	       	return ( true );
+	return ( false );	
+}
+
+searchable_array_bag::~searchable_array_bag(){}
---
+++ rank_5_exam/polyset/v04/set.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhvidste <rvidste@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/16 21:26:41 by rhvidste          #+#    #+#             */
+/*   Updated: 2026/02/16 21:38:59 by rhvidste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "set.hpp"
+#include "searchable_array_bag.hpp"
+
+set::set( searchable_bag& s_bag ) : bag( s_bag ) {}
+
+bool	set::has	( int value )	const
+{ return ( bag.has ( value ) ); }
+
+void	set::insert	( int value )
+{
+	if ( !(this->has ( value ) ) )
+		bag.insert( value );
+}
+
+void	set::insert	( int *data, int size )
+{
+	for ( int i = 0; i < size; i++ )
+		this->insert( data[i] );
+}
+
+void	set::print	()	const{ bag.print(); }
+void	set::clear	()	     { bag.clear(); } 
+
+const searchable_bag&	set::get_bag(){ return ( this->bag ); }
+
+set::~set(){}
